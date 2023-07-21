df4f9759d5cb86a46b573d57d096de6147768781
lilo <lilo.marino@hotmail.com>
2023-07-20 21:42:00 -0300

    Telas de Login e Cadastro

---
+++ Financeiro/cadastro.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+include_once '_head.php';
+?>
+<body>
+    <div class="container">
+        <div class="row text-center  ">
+            <div class="col-md-12">
+                <br /><br />
+                <h2> Controle Financeiro : Cadastro</h2>
+
+                <h5>( Faça seu cadastro )</h5>
+                <br />
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong> Preencher todos os campos </strong>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form">
+                            <br />
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-circle-o-notch"></i></span>
+                                <input type="text" class="form-control" placeholder="Seu Nome" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon">@</span>
+                                <input type="text" class="form-control" placeholder="Seu e-mail" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" class="form-control" placeholder="Crie uma senha (mínimo 6 caracteres)" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" class="form-control" placeholder="Repita a senha criada" />
+                            </div>
+
+                            <a href="index.html" class="btn btn-success ">Finalizar cadastro</a>
+                            <hr />
+                            Já possui um cadastro? <a href="login.php">Clique aqui</a>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ Financeiro/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php
+include_once '_head.php';
+?>
+
+<body>
+    <div class="container">
+        <div class="row text-center ">
+            <div class="col-md-12">
+                <br /><br />
+                <h2> Controle Financeiro : Login</h2>
+
+                <h5>( Faça seu login )</h5>
+                <br />
+            </div>
+        </div>
+        <div class="row ">
+
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong> Entre com seus dados </strong>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form">
+                            <br />
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-tag"></i></span>
+                                <input type="text" class="form-control" placeholder="Seu e-mail" />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" class="form-control" placeholder="Sua senha" />
+                            </div>
+
+                            <a href="index.html" class="btn btn-primary ">Login</a>
+                            <hr />
+                            Não possui conta? <a href="cadastro.php"> Clique aqui </a>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
