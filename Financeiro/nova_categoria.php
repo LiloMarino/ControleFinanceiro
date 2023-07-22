85ced35280463e929e7afc93796fe26651fd815b
lilo <lilo.marino@hotmail.com>
2023-07-22 11:46:27 -0300

    Tela Nova Categoria

---
+++ Financeiro/nova_categoria.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php
+include_once '_head.php'
+?>
+
+<body>
+    <div id="wrapper">
+
+        <?php
+        include_once '_topo.php';
+        include_once '_menu.php';
+        ?>
+
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Nova Categoria</h2>
+                        <h5>Aqui você poderá cadastrar todas as suas categorias!</h5>
+
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <hr />
+                <div class="form-group">
+                    <label>Nome da Categoria</label>
+                    <input class="form-control" placeholder="Digite o nome da categoria. Exemplo: Luz">
+                </div>
+                <button type="submit" class="btn btn-success">Adicionar</button>
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+</body>
+
+</html>
