5bd4e5d9a6711acf4992a5d3ffde6064ee8fb92d
Murilo <lilo.marino@hotmail.com>
2023-11-01 01:52:53 -0300

    Feito DAO da Categoria

--- DAO/Categoria.php
+++ DAO/Categoria.php
@@ -1,23 +1,61 @@
 <?php
+/**
+ * Categoria dos movimentos do sistema financeiro
+ */
 class Categoria
 {
-    public function cadastrarCategoria()
-    {
+    /**
+     * Nome da categoria
+     * @var string
+     */
+    public string $nome;
 
+    /**
+     * @param int $id Id da categoria no banco de dados, caso não passe nada como parâmetro é atribuído null
+     */
+    public function __construct(int $id = null)
+    {
+        if ($id != null) {
+            // Busca no banco e faz as atribuições
+        }
+        // Caso contrário interpreta-se que é um objeto que ainda não possui um id no banco
     }
 
-    public function consultarCategoria()
+    /**
+     * Cadastra a categoria no sistema
+     * @param string $nome Nome da categoria
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function cadastrarCategoria(string $nome): int
     {
-
+        return 1;
     }
 
-    public function atualizarCategoria()
+    /**
+     * Realiza a consulta de todas as categorias cadastradas
+     * @return array Retorna o array de Categorias[] 
+     */
+    static public function consultarCategoria(): array
     {
-
+        return [];
     }
 
-    public function excluirCategoria()
+    /**
+     * Atualiza as informações da categoria
+     * @param string $nome Nome da categoria
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function atualizarCategoria(string $nome): int
     {
-        
+        return 1;
+    }
+
+    /**
+     * Realiza a exclusão da categoria
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function excluirCategoria(): int
+    {
+        return 1;
     }
 }
