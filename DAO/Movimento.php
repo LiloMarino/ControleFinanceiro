7cd9bb2cc120498d92fec08d108756c141f77d00
Murilo <lilo.marino@hotmail.com>
2023-11-01 01:52:30 -0300

    Feito DAO do Movimento

--- DAO/Movimento.php
+++ DAO/Movimento.php
@@ -1,23 +1,109 @@
 <?php
+/**
+ * Movimento no sistema financeiro
+ */
 class Movimento
 {
-    public function realizarMovimento()
-    {
+    /**
+     * Tipo do movimento (Entrada/Saída)
+     * @var int 
+     */
+    public int $tipo;
+    /**
+     * Id da categoria do movimento
+     * @var int 
+     */
+    public int $idCategoria;
+    /**
+     * Data do movimento
+     * @var string 
+     */
+    public string $data;
+    /**
+     * Id da empresa do movimento
+     * @var int 
+     */
+    public int $idEmpresa;
+    /**
+     * Valor do movimento
+     * @var string 
+     */
+    public float $valor;
+    /**
+     * Id da conta bancária do movimento
+     * @var int 
+     */
+    public int $idConta;
+    /**
+     * Observação do movimento
+     * @var string 
+     */
+    public string $observacao;
 
+    /**
+     * @param int $id Id do movimento no banco de dados, caso não passe nada como parâmetro é atribuído null
+     */
+    public function __construct(int $id = null)
+    {
+        if ($id != null) {
+            // Busca no banco e faz as atribuições
+        }
+        // Caso contrário interpreta-se que é um objeto que ainda não possui um id no banco
     }
 
-    public function consultarMovimento()
+    /**
+     * Realiza o movimento inserindo-o no banco de dados
+     * @param int $tipo Tipo do movimento
+     * @param int $idCategoria Id da Categoria
+     * @param string $data Data do movimento
+     * @param int $idEmpresa Id da Empresa
+     * @param string $valor Valor do movimento
+     * @param int $idConta Id da conta
+     * @param string $observacao Observação do movimento
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function realizarMovimento(int $tipo, int $idCategoria, string $data, int $idEmpresa, string $valor, int $idConta, string $observacao = '') : int
     {
-
+        return 1;
     }
 
-    public function atualizarMovimento()
+    /**
+     * Consulta os movimentos presentes no banco em um dado intervalo de tempo
+     * @param int $tipo Tipo do movimento
+     * @param string $dataInicial Data inicial
+     * @param string $dataFinal Data final
+     * @return array Retorna um array de Movimentos[] 
+     */
+    static public function consultarMovimento(int $tipo, string $dataInicial, string $dataFinal): array
     {
-
+        return [];
     }
 
-    public function excluirMovimento()
+    /**
+     * Atualiza as informações do movimento
+     * @param int $tipo Tipo do movimento
+     * @param int $idCategoria Id da Categoria
+     * @param string $data Data do movimento
+     * @param int $idEmpresa Id da Empresa
+     * @param string $valor Valor do movimento
+     * @param int $idConta Id da conta
+     * @param string $observacao Observação do movimento
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function atualizarMovimento(int $tipo, int $idCategoria, string $data, int $idEmpresa, string $valor, int $idConta, string $observacao = ''): int
     {
-        
+        if (trim($valor) == '') {
+            return 0;
+        }
+        return 1;
+    }
+
+    /**
+     * Realiza a exclusão do movimento
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function excluirMovimento() : int
+    {
+        return 1;
     }
 }
