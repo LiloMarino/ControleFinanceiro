5a3f35b7ab48949dd4c21c433205d14948907280
Murilo <lilo.marino@hotmail.com>
2023-11-01 01:52:39 -0300

    Feito DAO da Empresa

--- DAO/Empresa.php
+++ DAO/Empresa.php
@@ -1,23 +1,75 @@
 <?php
+/**
+ * Empresa presente no movimento do sistema financeiro
+ */
 class Empresa
 {
-    public function cadastrarEmpresa()
-    {
+    /**
+     * Nome da empresa
+     * @var string 
+     */
+    public string $nome;
+    /**
+     * Telefone da empresa
+     * @var string
+     */
+    public string $telefone;
+    /**
+     * Endereço da empresa
+     * @var string
+     */
+    public string $endereco;
 
+    /**
+     * @param int $id Id da empresa no banco de dados, caso não passe nada como parâmetro é atribuído null
+     */
+    public function __construct(int $id = null)
+    {
+        if ($id != null) {
+            // Busca no banco e faz as atribuições
+        }
+        // Caso contrário interpreta-se que é um objeto que ainda não possui um id no banco
     }
 
-    public function consultarEmpresa()
+    /**
+     * Cadastra a empresa no sistema
+     * @param string $nome Nome da empresa
+     * @param string $telefone Telefone da empresa
+     * @param string $endereco Endereço da empresa
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function cadastrarEmpresa(string $nome, string $telefone = '', string $endereco = '') : int
     {
-
+        return 1;
     }
 
-    public function atualizarEmpresa()
+    /**
+     * Realiza a consulta de todas as empresas cadastradas
+     * @return array Retorna o array de Empresas[] 
+     */
+    static public function consultarEmpresa() : array
     {
-
+        return [];
     }
 
-    public function excluirEmpresa()
+    /**
+     * Atualiza as informações da empresa
+     * @param string $nome Nome da empresa
+     * @param string $telefone Telefone da empresa
+     * @param string $endereco Endereço da empresa
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function atualizarEmpresa(string $nome, string $telefone = '', string $endereco = '') : int
     {
-        
+        return 1;
     }
+
+    /**
+     * Realiza a exclusão da empresa
+     * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
+     */
+    public function excluirEmpresa() : int
+    {
+        return 1;
+    }   
 }
