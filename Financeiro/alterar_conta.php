5d75058cbf40695d675739cff8574edd285adcf7
Murilo <lilo.marino@hotmail.com>
2023-11-17 17:18:44 -0300

    Feito validação de conta em JS

--- Financeiro/alterar_conta.php
+++ Financeiro/alterar_conta.php
@@ -34,23 +34,28 @@ include_once '_head.php';
                 include_once '_msg.php';
                 ?>
                 <form action="alterar_conta.php" method="post">
-                    <div class="form-group">
-                        <label>Nome do Banco</label><span class="red-text">*</span>
-                        <input class="form-control" name="nome" placeholder="Digite o nome do banco. Exemplo: NuBank">
+                    <div id="divNome" class="form-group">
+                        <label for="nome">Nome do Banco</label><span class="red-text">*</span>
+                        <input id="nome" onblur="isCampoPreenchido(nome,divNome,false)" class="form-control" name="nome"
+                            placeholder="Digite o nome do banco. Exemplo: NuBank">
                     </div>
-                    <div class="form-group">
-                        <label>Agência</label><span class="red-text">*</span>
-                        <input class="form-control" name="agencia" placeholder="Digite a agência.">
+                    <div id="divAgencia" class="form-group">
+                        <label for="agencia">Agência</label><span class="red-text">*</span>
+                        <input id="agencia" onblur="isCampoPreenchido(agencia,divAgencia,false)" class="form-control"
+                            name="agencia" placeholder="Digite a agência.">
                     </div>
-                    <div class="form-group">
-                        <label>Número da Conta</label><span class="red-text">*</span>
-                        <input class="form-control" name="nconta" placeholder="Digite o número da conta.">
+                    <div id="divNconta" class="form-group">
+                        <label for="nconta">Número da Conta</label><span class="red-text">*</span>
+                        <input id="nconta" onblur="isCampoPreenchido(nconta,divNconta,false)" class="form-control"
+                            name="nconta" placeholder="Digite o número da conta.">
                     </div>
-                    <div class="form-group">
-                        <label>Saldo</label><span class="red-text">*</span>
-                        <input class="form-control" name="saldo" placeholder="Digite o saldo da conta.">
+                    <div id="divSaldo" class="form-group">
+                        <label for="saldo">Saldo</label><span class="red-text">*</span>
+                        <input id="saldo" onblur="isCampoPreenchido(saldo,divSaldo,false)" class="form-control"
+                            name="saldo" placeholder="Digite o saldo da conta.">
                     </div>
-                    <button type="submit" name="btn" class="btn btn-warning">Alterar</button>
+                    <button onclick="return ValidarCampos('nome','agencia','nconta','saldo')" type="submit" name="btn"
+                        class="btn btn-warning">Alterar</button>
                 </form>
             </div>
             <!-- /. PAGE INNER  -->
