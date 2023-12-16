5db0a08801cb44f541c3f5893a6eb8ccab9f3aac
Murilo <lilo.marino@hotmail.com>
2023-12-16 00:38:02 -0300

    Feito métodos de Categoria

--- DAO/Categoria.php
+++ DAO/Categoria.php
@@ -7,34 +7,29 @@ require_once 'Conexao.php';
 class Categoria
 {
     /**
+     * Id da categoria
+     * @var integer
+     */
+    public int $id_categoria;
+
+    /**
      * Nome da categoria
      * @var string
      */
-    public string $nome;
-
-    /**
-     * @param int $id Id da categoria no banco de dados, caso não passe nada como parâmetro é atribuído null
-     */
-    public function __construct(int $id = null)
-    {
-        if ($id != null) {
-            // Busca no banco e faz as atribuições
-
-        }
-        // Caso contrário interpreta-se que é um objeto que ainda não possui um id no banco
-    }
+    public string $nome_categoria;
 
     /**
      * Cadastra a categoria no sistema
+     * 
      * @param string $nome Nome da categoria
      * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
      */
-    public function cadastrarCategoria(string $nome): int
+    static public function cadastrarCategoria(string $nome): int
     {
         if (Util::isEmpty($nome)) {
             return 0;
         }
-        
+
         $query = "INSERT INTO categoria (nome_categoria, id_usuario) VALUES (?, ?)";
         $sql = Conexao::getConexao()->prepare($query);
         $sql->bindValue(1, $nome, PDO::PARAM_STR);
@@ -49,16 +44,32 @@ class Categoria
     }
 
     /**
-     * Realiza a consulta de todas as categorias cadastradas
-     * @return array Retorna o array de Categorias[] 
+     * Realiza a consulta das categorias cadastradas
+     *
+     * @param integer|null $id Id da Categoria
+     * @return Categoria|array Retorna Categoria|Categorias[] 
      */
-    static public function consultarCategoria(): array
+    static public function consultarCategoria(int $id = null): Categoria|array
     {
-        return [];
+        if ($id != null) {
+            // Busca no banco o objeto especificado e faz as atribuições
+            $query = "SELECT id_categoria, nome_categoria FROM categoria WHERE (id_categoria = ? AND id_usuario = ?)";
+            $sql = Conexao::getConexao()->prepare($query);
+            $sql->bindValue(1, $id, PDO::PARAM_INT);
+            $sql->bindValue(2, Util::codigoLogado(), PDO::PARAM_INT);
+            return $sql->fetch(PDO::FETCH_CLASS, 'Categoria');;
+        } else {
+            // Busca todos os elementos e retorna o array
+            $query = "SELECT id_categoria, nome_categoria FROM categoria WHERE (id_usuario = ?)";
+            $sql = Conexao::getConexao()->prepare($query);
+            $sql->bindValue(1, Util::codigoLogado(), PDO::PARAM_INT);
+            return $sql->fetchAll(PDO::FETCH_CLASS, 'Categoria');
+        }
     }
 
     /**
      * Atualiza as informações da categoria
+     * 
      * @param string $nome Nome da categoria
      * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
      */
@@ -67,15 +78,34 @@ class Categoria
         if (Util::isEmpty($nome)) {
             return 0;
         }
-        return 1;
+        $query = "UPDATE categoria SET (nome_categoria = ?) WHERE (id_categoria = ?)";
+        $sql = Conexao::getConexao()->prepare($query);
+        $sql->bindValue(1, $nome, PDO::PARAM_STR);
+        $sql->bindValue(1, $this->id_categoria, PDO::PARAM_INT);
+        try {
+            $sql->execute();
+            return 1;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return -1;
+        }
     }
 
     /**
      * Realiza a exclusão da categoria
+     * 
      * @return int Retorna 1 em caso de sucesso, 0 em caso de campos inválidos e -1 em caso de erros
      */
     public function excluirCategoria(): int
     {
-        return 1;
+        $query = "DELETE FROM categoria WHERE (id_categoria = ?)";
+        $sql = Conexao::getConexao()->prepare($query);
+        try {
+            $sql->execute();
+            return 1;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return -1;
+        }
     }
 }
